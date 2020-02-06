ea1f668fee591726c70b23f6fe04019553d989a7
Jimmy Hehir <jameshhehir@gmail.com>
2020-02-06 10:05:46 +0000

    Adding an example of do while loops

---
+++ DoWhile.java
@@ -0,0 +1,18 @@
+public class DoWhile {
+    public static void main(String args[]) {
+        int i=10;
+        do {
+            System.out.println(i);
+            i--;
+        }while (i>1);
+
+        int arr[] = { 1,4,5,2};
+        int j=0;
+        do {
+            System.out.println(arr[j]);
+            j++;
+        }while (j<4);
+
+
+    }
+}
